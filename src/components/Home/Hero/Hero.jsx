e7eaf2333051d7794d3bfc77c5b6ac20df7cdfb6
AB7zz <abhinavcv007@gmail.com>
2023-03-01 12:03:03 +0530

    some changes

--- src/components/Home/Hero/Hero.jsx
+++ src/components/Home/Hero/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
         <p className='text-[#2979F2] md:text-xl text-sm text-semibold'>IIIC SOE STUDENT - INDUSTRY JOB PORTAL</p>
         <h1 className='text-black font-bold text-2xl md:text-7xl'>Top Talents Awaits </h1>
         <h1 className='text-4xl md:text-5xl mt-4 font-bold'>Find your dream team here</h1>
-        <p className='mt-3 text-[#646464] text-sm'>Portal for Internships, Projects and Assistance</p>
-        <button to='/login' className='mt-12 rounded font-semibold text-sm bg-blue-500 pl-4 pr-4 px-3 py-3 text-white'>Discover the Best Minds Here</button>
+        <p className='mt-3 mb-12 text-[#646464] text-sm'>Portal for Internships, Projects and Assistance</p>
+        <Link to='/login' className='rounded font-semibold text-sm bg-blue-500 pl-4 pr-4 px-3 py-3 text-white'>Discover the Best Minds Here</Link>
       </div>
       <div className='hidden md:block'>
         <img src={cusat} alt="cusat" />
